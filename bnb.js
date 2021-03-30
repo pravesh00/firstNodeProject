3fce6a0bb9de49c10a324c96f607685a2c5ee41f
Pravesh Choudhary <praveshmails@gmail.com>
2021-03-30 23:06:24 +0530

    First

---
+++ bnb.js
@@ -0,0 +1,5 @@
+const mongoose=require('mongoose');
+
+const bnbSchema= mongoose.Schema({
+    name:String
+})
---
+++ index.js
@@ -0,0 +1,48 @@
+const express =require("express");
+const app=express();
+require('dotenv/config')
+const morgan=require('morgan');
+const mongoose=require('mongoose');
+
+
+
+
+const api=process.env.API_VER;
+
+app.use(express.json());
+app.use(morgan('tiny'));
+
+mongoose.connect(process.env.CONNECTION_STRING)
+.then(()=>{
+    console.log("Database Connected");
+});
+
+const bnbSchema= mongoose.Schema({
+    name:String
+});
+
+const bnbModel=mongoose.model('H',bnbSchema);
+
+app.get('/pro',(req,res)=>{
+    res.send("Chal na")
+})
+
+app.post('/pro',(req,res)=>{
+    const bnb = new bnbModel({
+        name:req.body.name
+    });
+    bnb.save().then((a)=>{
+        res.status(201).json(a);
+
+        console.log("Product Added")
+    }).catch((err)=>{
+        res.status(500).json({
+            error:err,
+            success:false
+        })
+    });
+})
+app.listen(3000,()=>{
+    console.log("Server started");
+    console.log(api);
+});
