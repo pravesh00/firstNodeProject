f2cb7ca7c4a63485e4ed83a9fa1e967e22a2f7ad
Pravesh Choudhary <praveshmails@gmail.com>
2021-03-31 00:05:13 +0530

    gtrg

--- index.js
+++ index.js
@@ -41,4 +41,7 @@ app.post('/pro',(req,res)=>{
             success:false
         })
     });
-})
+});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
